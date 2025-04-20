da0ec57f4a4df91712f33650cdafb080747825d8
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-04-20 15:28:06 +0500

    feat: Integrate Navbar component into Header
    - Added the Navbar component below the Topbar within the Header component.
    - Updated the Header component to use a `<header>` element instead of a fragment.

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 function Header() {
     return (
-        <>
+        <header>
             {/* Topbar */}
             <Topbar />
             {/* Navbar */}
+            <Navbar />
             {/* Cart Drawer */}
-        </>
+        </header>
     )
 }
 
---
+++ src/components/Common/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { HiOutlineShoppingBag, HiOutlineUsers } from 'react-icons/hi'
+import { HiBars3BottomRight } from 'react-icons/hi2'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <>
+            <nav className='container mx-auto flex justify-between py-4 px-6'>
+                {/* left - logo */}
+                <div>
+                    <Link to='/' className='text-2xl font-medium'>
+                        Shoppy
+                    </Link>
+                </div>
+                {/* center - Navigation Links */}
+                <div className='hidden md:flex space-x-6'>
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Men</Link>
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Women</Link>
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Top Wear</Link>
+                    <Link to='#' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Bottom Wear</Link>
+                </div>
+                {/* Right - Icons */}
+                <div className='flex items-center space-x-4'>
+                    <Link to="/profile" className='hover:text-black'>
+                        <HiOutlineUsers className='h-6 w-6 text-gray-700' />
+                    </Link>
+                    <button className='relative hover:text-black'>
+                        <HiOutlineShoppingBag className='h-6 w-6 text-gray-700 ' />
+                        <span className='absolute -top-1 bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5'>4</span>
+                    </button>
+                    {/* Search */}
+                    <button>
+                        <HiBars3BottomRight />
+                    </button>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
