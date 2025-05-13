87b52250c14f89678fc942f61193278c938c6591
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-05-13 14:41:20 +0500

    feat: Add register route and page
    - Added a register route to the App component.
    - Created a Register page component.
    - The register route is accessible under the /register path.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import UserLayout from './components/Layout/UserLayout'
 import Home from "./pages/Home";
 import { Toaster } from 'sonner';
 import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
   return (
     <BrowserRouter future={{ v7_startTransition: true }}>
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<UserLayout />}>
           <Route index element={<Home />} />
           <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
         </Route>
         <Route>
           {/* Admin Layout */}
