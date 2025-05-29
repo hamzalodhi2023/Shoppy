fa2bd4f55339de25136d84c36c01b25b92fa049c
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-05-29 09:38:27 +0500

    fix: Use `useSearchParams` instead of `useParams` in SortOptions
    - Replaced `useParams` with `useSearchParams` in the SortOptions component.
    - This change ensures that the component correctly reads and updates URL query parameters for sorting options.

--- src/components/Products/SortOptions.jsx
+++ src/components/Products/SortOptions.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
 
 function SortOptions() {
     const [searchParams, setSearchParams] = useSearchParams()
