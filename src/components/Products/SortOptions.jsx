dbab11ab156454663fa8ef284517b427df151953
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-05-29 09:35:06 +0500

    feat: Implement sorting functionality in SortOptions component
    - Added a select dropdown to the SortOptions component to allow users to sort products.
    - Implemented `handleSortChange` to update the URL parameters with the selected sorting option.
    - The component now includes options for sorting by default, price (low to high), price (high to low), and popularity.

--- src/components/Products/SortOptions.jsx
+++ src/components/Products/SortOptions.jsx
@@ -1,6 +1,22 @@
+import { useParams } from "react-router-dom"
+
 function SortOptions() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const handleSortChange = (e) => {
+        const sortBy = e.target.value;
+        searchParams.set("sortBy", sortBy)
+        setSearchParams(searchParams);
+    }
+
     return (
-        <div className="mb-4 "></div>
+        <div className="mb-4 flex items-center justify-end">
+            <select name="" id="sort" className="border p-2 rounded-md focus:outline-none">
+                <option onChange={handleSortChange} value={searchParams.get("sort")} className="">Default</option>
+                <option value="" className="priceAsc">Price: Low to High</option>
+                <option value="" className="priceDesc">Price: High to Low</option>
+                <option value="" className="popularity">Popularity</option>
+            </select>
+        </div>
     )
 }
 export default SortOptions
