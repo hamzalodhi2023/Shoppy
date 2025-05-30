e21ad4efce7ea8f689b72515241ef3bdb5a44f15
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-05-30 11:27:57 +0530

    feat: Add route for ProductDetails component
    - Added a new route in `App.jsx` for the `ProductDetails` component.
    - The route is defined as `product/:id`, allowing users to access product details by specifying the product ID in the URL.
    - This change enables navigation to individual product pages, enhancing the user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import CollectionPage from './pages/CollectionPage';
+import ProductDetails from './components/Products/ProductDetails';
 function App() {
   return (
     <BrowserRouter>
@@ -17,6 +18,7 @@ function App() {
           <Route path='register' element={<Register />} />
           <Route path='profile' element={<Profile />} />
           <Route path='collections/:collection' element={<CollectionPage />} />
+          <Route path="product/:id" element={<ProductDetails />} />
         </Route>
         <Route>
           {/* Admin Layout */}
