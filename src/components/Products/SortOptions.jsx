01e7c074def934c2ab389aa8e48133b5afc4c7ea
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-05-30 09:48:41 +0530

    refactor: Improve SortOptions component and fix onChange event
    - Moved the `onChange` event handler from the `option` elements to the `select` element in the `SortOptions` component.
    - Set the `value` prop on the `select` element to control the selected option based on the URL parameter "sortBy".
    - Added specific `value` attributes to each `option` element to correctly identify the selected sorting method.
    - Removed unnecessary className prop from option elements.
    - This change improves the functionality and usability of the SortOptions component by ensuring the correct sorting option is selected and that the `handleSortChange` function is triggered when the user selects a different option.

--- src/components/Products/SortOptions.jsx
+++ src/components/Products/SortOptions.jsx
@@ -9,11 +9,11 @@ function SortOptions() {
     }
     return (
         <div className="mb-4 flex items-center justify-end">
-            <select name="" id="sort" className="border p-2 rounded-md focus:outline-none">
-                <option onChange={handleSortChange} value={searchParams.get("sortBy") || ""} className="">Default</option>
-                <option value="" className="priceAsc">Price: Low to High</option>
-                <option value="" className="priceDesc">Price: High to Low</option>
-                <option value="" className="popularity">Popularity</option>
+            <select name="" id="sort" onChange={handleSortChange} value={searchParams.get("sortBy") || ""} className="border p-2 rounded-md focus:outline-none">
+                <option className="">Default</option>
+                <option value="priceAsc" className="priceAsc">Price: Low to High</option>
+                <option value="priceDesc" className="priceDesc">Price: High to Low</option>
+                <option value="popularity" className="popularity">Popularity</option>
             </select>
         </div>
     )
