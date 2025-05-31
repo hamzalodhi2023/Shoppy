72ae855678a28c64dea38b55c19bf002d79063e7
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-05-31 11:40:44 +0530

    feat: Implement checkout route and functionality in cart drawer
    - Added a new route for the Checkout component in App.jsx, accessible via /checkout.
    - Implemented navigation to the checkout page from the CartDrawer component.
    - Added an onClick handler to the "Checkout" button in CartDrawer that uses useNavigate to redirect the user to the /checkout route.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import CollectionPage from './pages/CollectionPage';
 import ProductDetails from './components/Products/ProductDetails';
+import Checkout from './components/Cart/Checkout';
 function App() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ function App() {
           <Route path='profile' element={<Profile />} />
           <Route path='collections/:collection' element={<CollectionPage />} />
           <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="checkout" element={<Checkout />} />
         </Route>
         <Route>
           {/* Admin Layout */}
--- src/components/Layout/CartDrawer.jsx
+++ src/components/Layout/CartDrawer.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import { IoMdClose } from 'react-icons/io'
 import CartComponents from '../Cart/CartComponents'
+import { useNavigate } from 'react-router-dom'
 
 function CartDrawer({ toggleCartDrawer, drawerOpen }) {
+    const navigate = useNavigate()
+    const handleCheckout = () => {
+        navigate("/checkout");
+    }
 
     return (
         <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
@@ -21,7 +26,7 @@ function CartDrawer({ toggleCartDrawer, drawerOpen }) {
 
             {/* Checkout button fixed at the bottom */}
             <div className='p-4 bg-white sticky bottom-0'>
-                <button className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 cursor-pointer transition'>Checkout</button>
+                <button onClick={handleCheckout} className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 cursor-pointer transition'>Checkout</button>
                 <p className='text-sm tracking-tighter text-gray-500 mt-2 text-center'>Shipping, texes, and discount codes calculated at checkout.</p>
             </div>
         </div>
