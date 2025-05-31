9bb5f42734d9f5ac3761ab2a8f5c6bdc2e42e168
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-05-31 12:19:22 +0530

    feat: Add dummy cart data for Checkout component
    - Added dummy cart data to the Checkout component for testing and development purposes.
    - The cart data includes an array of products with details such as name, size, color, price, and image URL.
    - Also includes a total price for the items in the cart.

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -1,3 +1,22 @@
+const cart = {
+    products: [
+        {
+            name: "stylish Jacket",
+            size: "M",
+            color: "Black",
+            price: 120,
+            image: "https://picsum.photos/150?random=1",
+        },
+        {
+            name: "casual Sneakers",
+            size: 42,
+            color: "White",
+            price: 80,
+            image: "https://picsum.photos/150?random=2",
+        }
+    ],
+    totalPrice: 195,
+}
 function Checkout() {
     return (
         <div>Checkout</div>
