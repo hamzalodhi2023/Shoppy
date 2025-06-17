4e316264b29e7013ecd4552b1a2ee5fb619d1a41
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-06-17 11:23:08 +0500

    feat: Implement Order Confirmation Page
    - Implemented a basic OrderConfirmationPage component.
    - Added dummy checkout data with sample order details.
    - The component currently returns a placeholder div.

---
+++ src/pages/OrderConfirmationPage.jsx
@@ -0,0 +1,36 @@
+const checkout = {
+    _id: "12323",
+    createdAt: new Date(),
+    checkoutItems: [
+        {
+            productId: "1",
+            name: "Jacket",
+            color: "Black",
+            size: "M",
+            price: 150,
+            quantity: 2,
+            image: "https://picsum.photos/150?random=1"
+        },
+        {
+            productId: "2",
+            name: "T-shirt",
+            color: "Black",
+            size: "M",
+            price: 120,
+            quantity: 1,
+            image: "https://picsum.photos/150?random=2"
+        },
+    ],
+    shippingAddress: {
+        address: "123 Fashion Streey"
+    }
+
+}
+
+
+function OrderConfirmationPage() {
+    return (
+        <div>OrderConfirmationPage</div>
+    )
+}
+export default OrderConfirmationPage
