5d95daf9c49f1260f163cd6d6602beb03fb5f48c
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-06-17 15:08:46 +0500

    feat: Enhance OrderConfirmationPage UI with thank you message
    - Added a thank you message to the OrderConfirmationPage.
    - Wrapped the content in a container with styling for better presentation.

--- src/pages/OrderConfirmationPage.jsx
+++ src/pages/OrderConfirmationPage.jsx
@@ -32,7 +32,10 @@ const checkout = {
 
 function OrderConfirmationPage() {
     return (
-        <div>OrderConfirmationPage
+        <div className="max-w-4xl mx-auto p-6 bg-white">
+            <h1 className="text-4xl font-bold text-center text-emerald-700 mb-8">Thank You for Your Order!</h1>
+            {checkout && <div className="p-6 rounded-lg border">
+            </div>}
         </div>
     )
 }
