655d7d442b87761a8df0ad81fc36c06114ec3da4
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-06-20 23:22:19 +0500

    feat: Fetch and display order details on OrderDetailsPage
    - Implemented the OrderDetailsPage component to fetch and display order details based on the order ID from the URL parameters.
    - Used `useParams` to extract the order ID.
    - Used `useState` and `useEffect` to manage and fetch order details.
    - Created mock order details to simulate fetching data from an API.
    - Displayed a placeholder div.

--- src/pages/OrderDetailsPage.jsx
+++ src/pages/OrderDetailsPage.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 function OrderDetailsPage() {
+    const { id } = useParams()
+    const [orderDetails, setOrderDetails] = useState(null)
+
+    useEffect(() => {
+        const mockOrderDetails = {
+            _id: id,
+            createdAt: new Date(),
+            isPaid: true,
+            isDelivered: false,
+            paymentMethod: "Paypal",
+            shippingMethod: "Standard",
+            shippingAddress: { city: "New Yourk", country: "USA" },
+            orderItems: [
+                {
+                    productId: "1",
+                    name: "Jacket",
+                    price: 120,
+                    quantity: 1,
+                    image: "https://picsum.photos/150?random=1"
+                },
+                {
+                    productId: "2",
+                    name: "Shirt",
+                    price: 150,
+                    quantity: 2,
+                    image: "https://picsum.photos/150?random=2"
+                },
+            ]
+        }
+        setOrderDetails(mockOrderDetails);
+    }, [id])
+
     return (
         <div></div>
     )
