b0edc47e44da134a917b40a71663dc3d5ec80b6c
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-06-25 11:28:56 +0500

    fix: Correct typo in UserManagement component heading
    - Corrected a typo in the UserManagement component's heading, changing "Usere Management" to "User Management".

--- src/components/Admin/UserManagement.jsx
+++ src/components/Admin/UserManagement.jsx
@@ -19,14 +19,14 @@ function UserManagement() {
 
 
     const handleChange = (e) => {
-    setFormData({
+        setFormData({
             ...formData,
             [e.target.name]: e.target.value,
         });
     }
     return (
         <div className="max-w-7xl mx-auto p-6">
-            <h2 className="text-2xl font-bold mb-4">Usere Management</h2>
+            <h2 className="text-2xl font-bold mb-4">User Management</h2>
             {/* Add New User Form */}
             <div className="p-t rounded-lg mb-6">
                 <h3 className="text-lg font-bold mb-4">Add New User</h3>
