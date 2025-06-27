ace65144ee4c42a19691fa75eebe85b2d96cdc0d
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-06-27 10:23:08 +0500

    feat: Implement basic ProductManagement component and fix typo in UserManagement
    - Implemented a basic `ProductManagement` component with a placeholder table for displaying products.
    - Added a heading and a table structure with columns for Name, Price, SKU, and Actions.
    - Corrected a typo in the `UserManagement` component, removing the extra "1" in the `div` with className "mb-4".

--- src/components/Admin/ProductManagement.jsx
+++ src/components/Admin/ProductManagement.jsx
@@ -1,6 +1,32 @@
 function ProductManagement() {
+    const products = [
+        {
+            _id: 123123,
+            name: "Shirt",
+            price: 100,
+            sku: "123123",
+
+        }
+    ]
     return (
-        <div>ProductManagement</div>
+        <div className="max-w-7xl mx-auto p-6">
+            <h2 className="text-2xl font-bold mb-6 ">Product Management</h2>
+            <div className="overflow-x-auto shadow-md md:rounded-lg">
+                <table className="min-w-full text-left text-gray-500">
+                    <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+                        <tr>
+                            <th className="py-3 px-4">Name</th>
+                            <th className="py-3 px-4">Price</th>
+                            <th className="py-3 px-4">SKU</th>
+                            <th className="py-3 px-4">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* {products.length > ? (): } */}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 export default ProductManagement
