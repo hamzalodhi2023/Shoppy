0d4f337dbe3798d1684018495401c6a513d3c77a
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-06-29 11:01:48 +0500

    feat: Implement basic form structure and state management in EditProductPage
    - Implemented a basic form structure in the `EditProductPage` component with fields for product name and description.
    - Added state management using the `useState` hook to handle product data, including name, description, price, countInStock, sku, category, brand, size, color, collections, material, gender, and images.
    - Implemented an `handleChange` function to update the product data state when the input fields change.
    - Added basic styling using Tailwind CSS classes for layout and appearance.

--- src/components/Admin/EditProductPage.jsx
+++ src/components/Admin/EditProductPage.jsx
@@ -1,6 +1,50 @@
+import { useState } from "react"
+
 function EditProductPage() {
+    const [productData, setProductData] = useState({
+        name: "",
+        description: "",
+        price: 0,
+        countInStock: 0,
+        sku: "",
+        category: "",
+        brand: "",
+        size: "",
+        color: [],
+        collections: "",
+        material: "",
+        gender: "",
+        images: [
+            {
+                url: "https://picsum.photos.150?random=1",
+            },
+            {
+                url: "https://picsum.photos.150?random=2",
+            }
+        ]
+
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProductData((prevData) => ({ ...prevData, [name]: value }))
+    }
     return (
-        <div>EditProductPage</div>
+        <div className="max-w-5xl mx-auto p-6 shadow-md rounded-md">
+            <h2 className="text-3xl font-bold mb-6">Edit Product</h2>
+            <form>
+                {/* Name */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Product Name</label>
+                    <input type="text" name="name" value={productData.name} onChange={handleChange} className="w-full border border-gray-300 rounded-md p-2" required />
+                </div>
+                {/* Description */}
+                <div className="mb-6">
+                    <label className="block font-semibold mb-2">Description</label>
+                    <textarea name="description" value={productData.description} className="w-full border border-gray-300 rounded-md p-2" rows={4} required />
+                </div>
+            </form>
+        </div>
     )
 }
 export default EditProductPage
