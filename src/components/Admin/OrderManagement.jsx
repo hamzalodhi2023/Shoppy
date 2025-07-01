0c7c9c9131e12a882c1b63d648628662c05cca8b
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-01 22:32:44 +0500

    feat: Add dummy order data to OrderManagement component
    - Added dummy order data to the `OrderManagement` component.
    - The data includes `_id`, `user` (with `name`), `totalPrice`, and `status` fields.
    - This dummy data will be used for testing and development purposes.

--- src/components/Admin/OrderManagement.jsx
+++ src/components/Admin/OrderManagement.jsx
@@ -1,4 +1,14 @@
 function OrderManagement() {
+  const orders = [
+    {
+      _id: 12312321,
+      user: {
+        name: "John Doe",
+      },
+      totalPrice: 100,
+      status: "Processing",
+    },
+  ];
   return <div>OrderManagement</div>;
 }
 export default OrderManagement;
