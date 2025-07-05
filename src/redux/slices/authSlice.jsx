91adbfdf50031414e417ff478e53bfbcdc337ab4
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-05 17:42:17 +0500

    feat: Setup Redux store and authSlice
    - Created a Redux store using Redux Toolkit's `configureStore`.
    - Implemented an `authSlice` to manage authentication state.
    - Included logic to retrieve user information from localStorage on initialization.

---
+++ src/redux/slices/authSlice.jsx
@@ -0,0 +1,7 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Retrieve user info and token from localStorage if available
+const userFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem(userInfo))
+  : null;
