ede0e0a2357176ea69e1f92b67314f6a9e5264d4
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-06 14:34:03 +0500

    feat: Implement guest ID generation and enhance authSlice
    - Implemented guest ID generation and storage in localStorage.
    - Added guestId to the initial state of the authSlice.
    - Added loginUser async thunk for handling user login API calls.

--- src/redux/slices/authSlice.jsx
+++ src/redux/slices/authSlice.jsx
@@ -3,5 +3,31 @@ import axios from "axios";
 
 // Retrieve user info and token from localStorage if available
 const userFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem(userInfo))
+  ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
+
+// Check for an existing guest ID in the localStorage or generate a new One
+
+const initialGuestId =
+  localStorage.getItem("guestId") || `guest_${new Date().getTime()}`;
+localStorage.setItem("guestId", initialGuestId);
+
+// Initial state
+const initialState = {
+  user: userFromStorage,
+  guestId: initialGuestId,
+  error: null,
+};
+
+// Async Thunk for User Login
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (userData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/user/login`,
+      );
+    } catch (error) {}
+  },
+);
