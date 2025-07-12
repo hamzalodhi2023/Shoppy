bf9627c5b19871c25f0749536f3cda312707ae9b
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-12 22:56:10 +0500

    feat: Implement productsSlice with fetchProductsByFilters async thunk
    - Implemented `productsSlice` with `fetchProductsByFilters` async thunk to fetch products based on various filters.
    - Added parameters for collection, size, color, gender, minPrice, maxPrice, sortBy, search, category, material, brand, and limit.
    - Constructed a URLSearchParams object to append filter parameters to the API request.

---
+++ src/redux/slices/productsSlice.jsx
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Async Thunk to Fetch Products by Collection and optional Filters
+
+export const fetchProductsByFilters = createAsyncThunk(
+  "products/fetchByFilters",
+  async ({
+    collection,
+    size,
+    color,
+    gender,
+    minPrice,
+    maxPrice,
+    sortBy,
+    search,
+    category,
+    material,
+    brand,
+    limit,
+  }) => {
+    const query = new URLSearchParams();
+    if (collection) query.append("collection", collection);
+    if (size) query.append("size", size);
+    if (color) query.append("color", color);
+    if (gender) query.append("gender", gender);
+    if (minPrice) query.append("minPrice", minPrice);
+    if (maxPrice) query.append("maxPrice", maxPrice);
+    if (sortBy) query.append("sortBy", sortBy);
+    if (search) query.append("search", search);
+    // if (category) query.append("category", category);
+    if (material) query.append("material", material);
+    if (brand) query.append("brand", brand);
+    if (limit) query.append("limit", limit);
+  },
+);
