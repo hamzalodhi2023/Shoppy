749df935762f76b6f682a5a563107eb43332f4a0
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-16 14:11:06 +0500

    fix: Correct import path for productsSlice in store.jsx
    - Corrected the import path for `productReducer` from `.slices/productsSlice` to `./slices/productsSlice` in `store.jsx`.

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import productReducer from ".slices/productsSlice";
+import productReducer from "./slices/productsSlice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
