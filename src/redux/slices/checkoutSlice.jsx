ef5fdc8760e162269cf5aa7cb61fc1407c4b7db9
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-22 14:48:35 +0500

    feat: Define createCheckout async thunk with basic structure
    - Defined the `createCheckout` async thunk using `createAsyncThunk` from Redux Toolkit.
    - Added a placeholder async function with `checkoutdata` and `rejectWithValue` parameters.
    - The thunk is currently a basic structure and will be further implemented in subsequent commits to handle checkout session creation logic.

--- src/redux/slices/checkoutSlice.jsx
+++ src/redux/slices/checkoutSlice.jsx
@@ -2,4 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 // Async thunk to create a checkout session
-export const createCheckout = createAsyncThunk();
+export const createCheckout = createAsyncThunk(
+  "checkout/createCheckout",
+  async (checkoutdata, { rejectWithValue }) => {},
+);
