5da04d28572ec9f2c612767fa6c0e9514660010d
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-22 14:43:07 +0500

    feat: Implement createCheckout async thunk in checkoutSlice
    - Implemented `createCheckout` async thunk using `createAsyncThunk` from Redux Toolkit.
    - Imported `axios` for making HTTP requests.
    - The `createCheckout` thunk will handle the creation of a checkout session. Further implementation will be added in subsequent commits.

---
+++ src/redux/slices/checkoutSlice.jsx
@@ -0,0 +1,5 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Async thunk to create a checkout session
+export const createCheckout = createAsyncThunk();
