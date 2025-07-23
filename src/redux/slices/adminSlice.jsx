6218c9e1d5fc137181087184a03964ab16b6f091
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-23 22:36:02 +0500

    fix: Return response data and handle errors in addUser async thunk

--- src/redux/slices/adminSlice.jsx
+++ src/redux/slices/adminSlice.jsx
@@ -28,7 +28,26 @@ export const addUser = createAsyncThunk(
           },
         },
       );
-      response.data;
-    } catch (error) {}
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Update user info
+export const updateUser = createAsyncThunk(
+  "admin/updateUser",
+  async ({ id, name, email, role }, { rejectWithValue }) => {
+    const response = await axios.put(
+      `${import.meta.env.VITE_BACKEND_URL}/api/admin/users/${userId}`,
+      { name, email, role },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      },
+    );
+    response.data;
   },
 );
