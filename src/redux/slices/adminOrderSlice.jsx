ecfe401fb3dfa48ba207f7df89df7b94fdc9ce5e
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-07-25 23:57:18 +0500

    feat: Add API_URL constant and define fetchAllOrders async thunk in adminOrderSlice

--- src/redux/slices/adminOrderSlice.jsx
+++ src/redux/slices/adminOrderSlice.jsx
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}`;
 // Fetch all orders (admin only)
+export const fetchAllOrders = createAsyncThunk();
