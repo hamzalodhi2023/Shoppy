dfdc869e28fb71e2b45bdf20bb7e50a3dd4019ff
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-08-17 15:49:46 +0500

    order confirmation has been completed

--- src/pages/OrderConfirmationPage.jsx
+++ src/pages/OrderConfirmationPage.jsx
@@ -26,7 +26,8 @@ function OrderConfirmationPage() {
   return (
     <div className="mx-auto max-w-4xl bg-white p-6">
       <h1 className="mb-8 text-center text-4xl font-bold text-emerald-700">
-        Thank You for Your Order!
+        {/* Thank You for Your Order! */}
+        3421 Agay dekh aur chalta ban...
       </h1>
       {checkout && (
         <div className="rounded-lg border p-6">
