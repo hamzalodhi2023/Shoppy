bd827461fdbf03e73ff42c8f9e0250d8a8808afd
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2025-08-26 13:41:08 +0500

    clg

--- src/components/Cart/CartComponents.jsx
+++ src/components/Cart/CartComponents.jsx
@@ -30,71 +30,76 @@ function CartComponents({ cart, userId, guestId }) {
   };
   return (
     <div>
-      {cart.products.map((product, index) => (
-        <div
-          key={index}
-          className="flex items-start justify-between border-b py-4"
-        >
-          <div className="flex items-start">
-            <img
-              src={product.image}
-              alt={product.name}
-              className="mr-4 h-24 w-20 rounded object-cover"
-            />
-            <div>
-              <h3>{product.name}</h3>
-              <p className="text-sm text-gray-500">
-                size:{product.size} | color: {product.color}
-              </p>
-              <div className="mt-2 flex items-center">
+      {cart.products.map(
+        (product, index) => (
+          console.log(product),
+          (
+            <div
+              key={index}
+              className="flex items-start justify-between border-b py-4"
+            >
+              <div className="flex items-start">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="mr-4 h-24 w-20 rounded object-cover"
+                />
+                <div>
+                  <h3>{product.name}</h3>
+                  <p className="text-sm text-gray-500">
+                    size:{product.size} | color: {product.color}
+                  </p>
+                  <div className="mt-2 flex items-center">
+                    <button
+                      onClick={() =>
+                        handleAddToCart(
+                          product.productId,
+                          -1,
+                          product.quantity,
+                          product.size,
+                          product.color,
+                        )
+                      }
+                      className="cursor-pointer rounded border px-2 py-1 text-xl font-medium"
+                    >
+                      -
+                    </button>
+                    <span className="mx-4">{product.quantity}</span>
+                    <button
+                      onClick={() =>
+                        handleAddToCart(
+                          product.productId,
+                          1,
+                          product.quantity,
+                          product.size,
+                          product.color,
+                        )
+                      }
+                      className="cursor-pointer rounded border px-2 py-1 text-xl font-medium"
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <p>${product.price.toLocaleString()}</p>
                 <button
                   onClick={() =>
-                    handleAddToCart(
+                    handleRemoveFromCart(
                       product.productId,
-                      -1,
-                      product.quantity,
                       product.size,
                       product.color,
                     )
                   }
-                  className="cursor-pointer rounded border px-2 py-1 text-xl font-medium"
                 >
-                  -
-                </button>
-                <span className="mx-4">{product.quantity}</span>
-                <button
-                  onClick={() =>
-                    handleAddToCart(
-                      product.productId,
-                      1,
-                      product.quantity,
-                      product.size,
-                      product.color,
-                    )
-                  }
-                  className="cursor-pointer rounded border px-2 py-1 text-xl font-medium"
-                >
-                  +
+                  <RiDeleteBin2Line className="mt-2 h-6 w-6 cursor-pointer text-red-600" />
                 </button>
               </div>
             </div>
-          </div>
-          <div>
-            <p>${product.price.toLocaleString()}</p>
-            <button
-              onClick={() =>
-                handleRemoveFromCart(
-                  product.productId,
-                  product.size,
-                  product.color,
-                )
-              }
-            >
-              <RiDeleteBin2Line className="mt-2 h-6 w-6 cursor-pointer text-red-600" />
-            </button>
-          </div>
-        </div>
-      ))}
+          )
+        ),
+      )}
     </div>
   );
 }
